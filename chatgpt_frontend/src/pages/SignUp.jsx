92453a951a3866b8e5880e5ac0076f984c5598b2
Muhammed Nishad c <122031511+mhdnishadc@users.noreply.github.com>
2025-10-06 13:51:12 +0530

    Disable CSRF for API endpoints

--- chatgpt_frontend/src/pages/SignUp.jsx
+++ chatgpt_frontend/src/pages/SignUp.jsx
@@ -10,7 +10,7 @@ function SignUp() {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      await API.post("api/signup/", {
+      await API.post("/api/signup/", {
         username,
         password,
       });
--- chatgpt_frontend/src/services/api.js
+++ chatgpt_frontend/src/services/api.js
@@ -1,16 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/", // your Django backend
+  baseURL: "https://chat-ai-1-p4go.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// attach token
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
 
 export default API;
