178a12042443cb1fee703d70e9a3ac47253f47d0
Muhammed Nishad c <122031511+mhdnishadc@users.noreply.github.com>
2025-10-06 11:45:54 +0530

    deploying issues

--- chatgpt_backend/settings.py
+++ chatgpt_backend/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,9 +63,9 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173", 
-]
+# CORS settings - Simple approach for personal projects
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
